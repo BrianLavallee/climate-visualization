7b0ff8ddfda69b408c3365c2cb8211676f8eb0b2
Daniel Merrell <daniel.r.merrell@gmail.com>
2019-11-05 18:00:46 -0700

    draw year slider

---
+++ vis/js/country-view.js
@@ -0,0 +1,69 @@
+
+class CountryView {
+
+    constructor() {
+        this.activeMeters = 0;
+        this.sliderWidthPX = 425;
+    }
+
+
+    drawYearBar() {
+
+        this.activeMeters = 2;
+
+
+        d3
+            .select('#CountryView')
+            .append('div')
+            .attr('id', 'CountryInfo');
+
+        d3
+            .select('#CountryView')
+            .append('div')
+            .attr('id', 'CountryImage');
+
+        d3
+            .select('#CountryView')
+            .append('div')
+            .attr('id', 'Slider');
+
+        //Slider to change the activeYear of the data
+        this.yearScale = d3.scaleLinear().domain([0, 5]).range([0, this.sliderWidthPX]);
+        let sliderTextScale = d3.scaleLinear().domain([0, 5]).range([12, this.sliderWidthPX - 10]);
+
+        let yearSlider = d3.select('#Slider')
+            .append('div').classed('slider-wrap', true)
+            .append('input').classed('slider', true)
+            .attr('type', 'range')
+            .attr('min', 0)
+            .attr('max', 5)
+            .attr('value', this.activeMeters);
+
+        let sliderLabel = d3.select('.slider-wrap')
+            .append('div').classed('slider-label', true)
+            .append('svg');
+
+        let sliderText = sliderLabel.append('text')
+            .text(`${this.activeMeters}m`)
+            .attr('id', 'SliderText');
+
+        sliderText.attr('x', sliderTextScale(this.activeMeters));
+        sliderText.attr('y', 25);
+
+        yearSlider.on('input', (_, __, sliderArr) => {
+
+            // TODO update table data
+
+            this.activeMeters = sliderArr[0].value;
+
+            // update year slider text
+            d3
+                .select('#SliderText')
+                .attr('x', sliderTextScale(this.activeMeters))
+                .text(`${this.activeMeters}m`);
+        });
+    }
+}
+
+
+
--- vis/js/script.js
+++ vis/js/script.js
@@ -3,4 +3,7 @@ d3.csv('data/area_impact.csv').then(data => {
 
     let table = new Table();
     table.createTable(data);
+
+    let countryView = new CountryView();
+    countryView.drawYearBar();
 });
